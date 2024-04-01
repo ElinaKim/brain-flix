971eb5d8d8cb8a2a790f591f6c916cd88818e242
Elina Kim <elinakim791@gmail.com>
2024-04-01 14:56:02 -0700

    implement video list component

---
+++ src/components/VideoList/VideoList.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import './VideoList.scss'
+import video from '../../data/videos.json'
+import videoDetails from '../../data/video-details.json'
+import Video from '../../components/Video/Video'
+
+export default function VideoList(){
+    const [selectedVideo, setSelectedVideo] = useState(videoDetails[0])
+
+    const handleVideoClick = (video) => {
+        setSelectedVideo(video);
+
+    }
+    const selectedVideoDetails = videoDetails.find(item => item.id === selectedVideo)
+    
+    const filteredVideos = video.filter((item) => selectedVideo.id !== item.id)
+    return(
+    <div className='videoList'>
+        <Video selectedVideo={selectedVideoDetails || videoDetails[0]}/>
+        <h2 className='videoList__heading'>NEXT VIDEOS</h2>
+        {filteredVideos.map((video)=>(
+            <div className='video' key={video.id} onClick={() => handleVideoClick(video.id)}>
+                <img className='video__img' src={video.image} alt={video.title}></img>
+                <div className='details'>
+                    <h3 className='details__title'>{video.title}</h3>
+                    <p className='details__author'>{video.channel}</p>
+            </div>
+        </div>
+        ))}
+
+    </div>
+    )
+}
