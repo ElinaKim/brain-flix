2fd146753c22cd6898f9c58817dfe416aaa22c18
Elina Kim <elinakim791@gmail.com>
2024-04-01 21:17:51 -0700

    remove videoPlayer to make it a seperate component

--- src/components/Video/Video.jsx
+++ src/components/Video/Video.jsx
@@ -8,7 +8,6 @@ export default function Video({ selectedVideo }){
 
     return(
         <div className='videoPreview'>
-            <video className='videoPreview__video' controls poster={image}></video>
             <h2 className='videoPreview__title'>{title}</h2>
             <div className='videoDetails'>
             <div className='videoDetails__container'>
