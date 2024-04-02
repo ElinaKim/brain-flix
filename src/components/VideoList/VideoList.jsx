ac4623e9bfaa7de995d3360ed87c63b49f211f78
Elina Kim <elinakim791@gmail.com>
2024-04-01 17:17:38 -0700

    pass selected video's comment array

--- src/components/VideoList/VideoList.jsx
+++ src/components/VideoList/VideoList.jsx
@@ -3,6 +3,7 @@ import './VideoList.scss'
 import video from '../../data/videos.json'
 import videoDetails from '../../data/video-details.json'
 import Video from '../../components/Video/Video'
+import Comment from '../../components/Comment/Comment'
 
 export default function VideoList(){
     const [selectedVideo, setSelectedVideo] = useState(videoDetails[0])
@@ -12,11 +13,12 @@ export default function VideoList(){
 
     }
     const selectedVideoDetails = videoDetails.find(item => item.id === selectedVideo)
-    
     const filteredVideos = video.filter((item) => selectedVideo.id !== item.id)
+    
     return(
     <div className='videoList'>
         <Video selectedVideo={selectedVideoDetails || videoDetails[0]}/>
+        <Comment comments={selectedVideoDetails.comments|| videoDetails[0].comments}/>
         <h2 className='videoList__heading'>NEXT VIDEOS</h2>
         {filteredVideos.map((video)=>(
             <div className='video' key={video.id} onClick={() => handleVideoClick(video.id)}>
