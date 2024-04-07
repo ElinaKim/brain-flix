693e2e65104eb4f659739de57fd359053374fd7d
Elina Kim <elinakim791@gmail.com>
2024-04-07 15:14:22 -0700

    implement routing and modify importing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import './styles/main.scss'
 import Header from './components/Header/Header'
-import VideoList from './components/VideoList/VideoList'
+import VideoPlayer from './components/VideoPlayer/VideoPlayer'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Upload from './pages/Upload/Upload'
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <VideoList/>
+      <BrowserRouter>
+      <Routes>
+      <Route element={<Header/>}></Route>
+      <Route element={<Header />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/upload" element={<Upload />} />
+        <Route path="/videos/:videoId" element={<VideoPlayer />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
