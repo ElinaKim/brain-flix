24bf1bd3912ba2a0245ab7808a77418f500af5e5
Elina Kim <elinakim791@gmail.com>
2024-04-07 17:22:42 -0700

    implement page not found

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,18 @@
 import './styles/main.scss'
-import Header from './components/Header/Header'
-import VideoPlayer from './components/VideoPlayer/VideoPlayer'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Upload from './pages/Upload/Upload'
+import NotFound from './pages/NotFound/NotFound'
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <Routes>
-      <Route element={<Header/>}></Route>
-      <Route element={<Header />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/upload" element={<Upload />} />
-        <Route path="/videos/:videoId" element={<VideoPlayer />} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/upload" element={<Upload />} />
+          <Route path="/videos/:videoId" element={<Home />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </div>
