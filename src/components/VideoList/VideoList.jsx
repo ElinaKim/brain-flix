fef8569fa9da1c31749aede2ea3b3f5535092d36
Elina Kim <elinakim791@gmail.com>
2024-04-07 17:24:22 -0700

    link to video player page by id

--- src/components/VideoList/VideoList.jsx
+++ src/components/VideoList/VideoList.jsx
@@ -1,21 +1,24 @@
 import './VideoList.scss'
 import '../../pages/Home/Home.scss'
+import {useParams, Link} from 'react-router-dom'
 
 export default function VideoList({video, selectedVideo, handleVideoClick}) {
     const filteredVideos = video.filter((item) => selectedVideo.id !== item.id);
-
+    const params = useParams();
     return(
             <div className='videoList'>
                 <h2 className='videoList__heading'>NEXT VIDEOS</h2>
                 {
                     filteredVideos.map((video)=>(
+                    <Link to={`/videos/${video.id}`}>
                         <div className='video' key={video.id} onClick={() => handleVideoClick(video)}>
-                            <img className='video__img' src={video.image} alt={video.title}></img>
+                          <img className='video__img' src={video.image} alt={video.title}/>
                             <div className='details'>
                                 <h3 className='details__title'>{video.title}</h3>
                                 <p className='details__author'>{video.channel}</p>
+                            </div>
                         </div>
-                    </div>
+                    </Link>
                     ))
                 }
             </div>
