feb5f5f754fe8eb496060f2b4e108b073314f6a4
Elina Kim <elinakim791@gmail.com>
2024-04-07 17:23:12 -0700

    upload link and logo link

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,23 @@
 import './Header.scss';
 import logoImage from '../../assets/images/BrainFlix-logo.svg'
-// import searchIcon from '../../assets/icons/search.svg'
-import uploadIcon from '../../assets/icons/upload.svg'
 import userImg from '../../assets/images/Mohan-muruge.jpg'
+import { Link } from 'react-router-dom'
 export default function Header(){
     return(
     <div className="header">
-    <img className="header__logo" src={logoImage} alt='brand logo'/>
+        <Link to='/'>
+            <img className="header__logo" src={logoImage} alt='brand logo'/>
+        </Link>
             <div className='search__container'>
-            <div className="search__input">
+                <div className="search__input">
                 <input className="search__field" type="text" placeholder='Search'/>
             </div>
             <div className='search__avatar-cropper'>
                 <img className='search__avatar' src={userImg} alt='user avatar'></img>
             </div>
-            <button className='search__upload'>UPLOAD</button>
+            <Link to='/upload'>
+                <button className='search__upload'>UPLOAD</button>
+            </Link>
             </div>
         </div>
     )
