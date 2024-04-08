014addc3558a812bc615ca9f1b38928862f02618
Elina Kim <elinakim791@gmail.com>
2024-04-07 17:02:20 -0700

    implement not found page

---
+++ src/pages/NotFound/NotFound.jsx
@@ -0,0 +1,9 @@
+export default function NotFound(){
+    return(
+        <div style={{display: 'flex', alignItems: 'center', flexDirection: 'column'}}>
+            <h2>Oops!</h2>
+            <h3>404 - Page not found</h3>
+            <h3>Please return to home page 🎬</h3>
+        </div>
+    )
+}
