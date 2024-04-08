77cbdd987628f9bc363c90cd076fb7d476f814a4
Elina Kim <elinakim791@gmail.com>
2024-04-07 22:59:57 -0700

    implement form submission logic

--- src/pages/Upload/Upload.jsx
+++ src/pages/Upload/Upload.jsx
@@ -1,13 +1,28 @@
 import Header from '../../components/Header/Header'
 import thumbnailImg from '../../assets/images/Upload-video-preview.jpg'
 import './Upload.scss'
+import { useNavigate } from 'react-router-dom'
+
 export default function Upload(){
+    const navigate = useNavigate()
+    const handleFormSubmit = (event) => {
+        event.preventDefault()
+        const form = event.target
+        const title = form.title.value
+        const description = form.description.value
+        if(!title || !description){
+            alert('Please enter your title and your description!')
+            return
+        }
+        alert('Video uploaded successfully!')
+        navigate('/') 
+    }
     return(
         <div>
             <Header />
             <div className='upload'>
                 <h2 className='upload__title'>Upload Video</h2>
-                    <form className='form__upload'>
+                    <form onSubmit={handleFormSubmit} className='form__upload'>
                         <div className='form__body'>
                             <div className='thumbnail'>
                                 <h3 className='thumbnail__title'>VIDEO THUMBNAIL</h3>
@@ -16,9 +31,9 @@ export default function Upload(){
                             <div className='form__textContent'>
                                 <div className='vidTitle'>
                                     <h3 className='vidTitle__title'>TITLE YOUR VIDEO</h3>
-                                    <textarea className='vidTitle__input' name='title'
+                                    <input className='vidTitle__input' name='title'
                                     placeholder='Add a title to your video'>
-                                    </textarea>
+                                    </input>
                                 </div>
                                 <div className='description'>
                                     <h3 className='description__title'>ADD A VIDEO DESCRIPTION</h3>
@@ -29,7 +44,10 @@ export default function Upload(){
                             </div>
                         </div>
                         <div className='btn'>
-                                <button className='btn__publish' type='submit'>PUBLISH</button>
+                                <button 
+                                className='btn__publish'
+                                >PUBLISH
+                                </button>
                                 <button className='btn__cancel'>CANCEL</button>
                         </div>
                     </form>
