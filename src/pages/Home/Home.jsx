b11fc48cca7f7941259e7c2fc2b6cb91e57f5953
Elina Kim <elinakim791@gmail.com>
2024-04-08 14:10:25 -0700

    fetch comments by id from api

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,12 +7,14 @@ import Video from '../../components/Video/Video'
 import Comment from '../../components/Comment/Comment'
 import VideoList from '../../components/VideoList/VideoList'
 import axios from 'axios'
+import {useParams} from 'react-router-dom'
 
 const API_KEY = process.env.REACT_APP_API_KEY
 
 export default function Home() {
+    const { videoId } = useParams()
     const [selectedVideo, setSelectedVideo] = useState(null);
-    const [selectedVideoDetails, setSelectedVideoDetails] = useState(videoDetails.find(item => item.id === videoDetails[0].id) || null);
+    const [selectedVideoDetails, setSelectedVideoDetails] = useState({});
     const [videos, setVideos] = useState([])
 
     useEffect(()=>{
@@ -28,33 +30,45 @@ export default function Home() {
         fetchVideos()
     },[])
 
+    useEffect(()=>{
+        const fetchVideosById = async()=> {
+            try{
+            const response = await axios.get(`https://unit-3-project-api-0a5620414506.herokuapp.com/videos/${videoId}?api_key=${API_KEY}`)
+            setSelectedVideoDetails(response.data)
+        } catch (error) {
+             console.error('Unable to get video details: ', error)
+        }
+    }
+        fetchVideosById()
+    },[videoId, API_KEY])
+
     const handleVideoClick = (video) => {
         if (!video.id) {
             throw new Error('Invalid video');
         }
         setSelectedVideo(video);
-        setSelectedVideoDetails(videoDetails.find(item => item.id === video.id));
+        setSelectedVideoDetails(video);
     }
 
     return(
     <div className='home'>
-    <Header />
-    <div className='videoMain'>
-        <VideoPlayer selectedVideo={selectedVideoDetails} />
-        <div className='videoComponent'>
-            <div className='videoComponent__video'>
-                <Video selectedVideo={selectedVideoDetails} />
-                <Comment comments={selectedVideoDetails.comments} />
-            </div>
-            <div className='videoComponent__videoList'>
-                <VideoList
-                video={videos}
-                selectedVideo={selectedVideo}
-                handleVideoClick={handleVideoClick}
-                />
+        <Header />
+        <div className='videoMain'>
+            <VideoPlayer selectedVideo={selectedVideoDetails} />
+            <div className='videoComponent'>
+                <div className='videoComponent__video'>
+                    <Video selectedVideo={selectedVideoDetails} />
+                    <Comment comments={selectedVideoDetails.comments} />
+                </div>
+                <div className='videoComponent__videoList'>
+                    <VideoList
+                    video={videos}
+                    selectedVideo={selectedVideo}
+                    handleVideoClick={handleVideoClick}
+                    />
+                </div>
             </div>
         </div>
-    </div>
     </div>
     )
 }
