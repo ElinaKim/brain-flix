ef7876722ac5f457f40ec443b8046545609fdf76
Elina Kim <elinakim791@gmail.com>
2024-04-08 22:20:50 -0700

    handle error if selectedVideo is null

--- src/components/VideoPlayer/VideoPlayer.jsx
+++ src/components/VideoPlayer/VideoPlayer.jsx
@@ -1,9 +1,13 @@
 import './VideoPlayer.scss'
 
 export default function VideoPlayer({selectedVideo}){
+    if (!selectedVideo) {
+        return null
+    }
+    
     return(
-    <div className='videoPlayer'>
-    <video className='videoPlayer__video' controls poster={selectedVideo.image}></video>
-    </div>
+        <div className='videoPlayer'>
+            <video className='videoPlayer__video' controls poster={selectedVideo.image}></video>
+        </div>
     )
 }
